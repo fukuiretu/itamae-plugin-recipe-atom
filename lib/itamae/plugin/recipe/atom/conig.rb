08e9828c3b7ae1e13a0f20fea2140dd081c2b683
fukuiretu <s0232101@gmail.com>
2015-02-24 16:47:26 +0900

    Add config recipe

--- lib/itamae/plugin/recipe/atom/conig.rb
+++ lib/itamae/plugin/recipe/atom/conig.rb
@@ -1,13 +1,22 @@
 ATOM_DIR = '~/.atom'
 
-node['atom']['configs'].each do |config|
-  config_file = File.basename(config)
+%W(
+  config
+  keymap
+  snippets
+  styles
+).each do |v|
+  from_config_file_path = node[:atom][:configs][v]
+  next if from_config_file_path.nil?
+
+  config_file_name = v == 'snippets' ? "#{v}.css" : "#{v}.cson"
 
   execute "backup origin file #{config_file}" do
-    command "cp #{ATOM_DIR}/#{config_file} #{ATOM_DIR}/#{config_file}.bk"
+    command "cp #{ATOM_DIR}/#{config_file_name} #{ATOM_DIR}/#{config_file_name}.bk"
   end
 
-  execute "backup origin file #{config_file}" do
-    command "cp #{config} #{ATOM_DIR}"
+
+  execute "setting #{config_file_name}" do
+    command "cp #{from_config_file_path} #{ATOM_DIR}/#{config_file_name}"
   end
 end
